a62f7becbbcb64e1fceb034d86dac9809397a28b
mdmostafa <mdmostafaalmahmud@gmail.com>
2021-02-24 22:11:49 +0600

    connecting db

---
+++ db/dbConnection.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const mongoDB =  `http://localhost:27017/ecomDB`
+// const mongoDB =  'mongodb://127.0.0.1/ecomDB'
+
+// Connecting database
+module.exports.connectDB = async () => {
+    try {
+        await mongoose.connect(
+            mongoDB,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false,
+                useCreateIndex: true
+            },
+            () => console.log('database connected successfully'))
+    } catch (err) {
+        console.error(err)
+    }
+}
